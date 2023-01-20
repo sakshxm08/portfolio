f1f36102e17cfa4c9042a8ad2e3a4cbda550de28
Saksham Gambhir <100534890+sakshxm08@users.noreply.github.com>
2023-01-20 23:07:58 +0530

    Designed HomePage

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-document.querySelector(".hamburger").addEventListener("click", () => {
-  document.querySelector(".sidebar").classList.toggle("sidebarTran");
-  document.querySelector(".cross").classList.toggle("d-none");
-  document.querySelector(".menu").style.display = "none";
-});
+// document.querySelector(".hamburger").addEventListener("click", () => {
+//   document.querySelector(".sidebar").classList.toggle("sidebarTran");
+//   document.querySelector(".cross").classList.toggle("d-none");
+//   document.querySelector(".menu").style.display = "none";
+// });
+
+// Get HTML head element
+
+// let theme = document.getElementById("theme");
+// let themeBtn = document.getElementById("themeBtn");
+
+// const themeChecked = () => {
+//   theme.checked = false;
+// };
+// themeBtn.addEventListener("click", themeChecked());
+// if (theme.checked) {
+//   link.href = "style.css";
+// }
+
+// const themeFn = () => {
+//   const head = document.getElementsByTagName("HEAD")[0];
+
+//   // Create new link Element
+//   const link = document.createElement("link");
+
+//   // set the attributes for link element
+//   link.rel = "stylesheet";
+
+//   link.type = "text/css";
+
+//   link.href = "style.css";
+
+//   // Append link element to HTML head
+//   head.appendChild(link);
+// };
